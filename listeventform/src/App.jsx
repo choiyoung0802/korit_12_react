4639b65826f1441458f77a8d268eefadebd5020c
choiyoung0802 <mj12579632@gmail.com>
2026-03-04 17:46:45 +0900

    feat: 20260304 useState / Effect / Ref / Custom Hooks / ContextAPI

--- listeventform/src/App.jsx
+++ listeventform/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
+import MyList from './Mylist';
 
 function App() {
   
 
   return (
     <>
-      
+      <MyList/>
     </>
-  )
+  );
 }
 export default App
--- listeventform/src/MyList.jsx
+++ listeventform/src/MyList.jsx
@@ -1,3 +1,13 @@
 export default function MyList() {
-  
+  const data = [1,2,3,4,5];
+
+  return (
+    <ul>
+      {
+        data.map(elem => 
+          <li>List Item : {elem*2}</li>
+        )
+      }
+    </ul>
+  );
 }
