dff91450d29e9897593b473a05805e688327939e
choiyoung0802 <mj12579632@gmail.com>
2026-03-06 17:45:49 +0900

    feat: 20260306 multiple() / TypeScript Basic

---
+++ statetask/src/Users.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function Users() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = event => {
+    alert(`Hello ${username}!`);
+    event.preventDefault();
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Username</label> <br />
+      <input type="text" onChange={event => setUsername(event.target.value)} value={username}/> <br />
+      <label>Password</label> <br />
+      <input type="password" onChange={event => setPassword(event.target.value)} value={password}/> <br />
+      <label>E-mail</label> <br />
+      <input type="email" onChange={event => setEmail(event.target.value)} value={email}/> <br />
+      <input type="submit" />
+    </form>
+  );
+}
