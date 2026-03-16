edcefe23b0e2c1cf24f0a42def1a3b9604d0dffb
choiyoung0802 <mj12579632@gmail.com>
2026-03-16 12:14:03 +0900

    feat: carfront2 copied

---
+++ carfront2/src/components/Carlist.tsx
@@ -0,0 +1,88 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { getCars, deleteCar } from "../api/Carapi";
+import { DataGrid, GridCellParams, GridColDef, GridToolbar } from "@mui/x-data-grid";
+import { Snackbar, Button } from "@mui/material";
+import { useState } from "react";
+import AddCar from "./AddCar";
+import EditCar from "./EditCar";
+
+export default function Carlist() {
+  const [open, setOpen] = useState(false);
+  const queryClient = useQueryClient();
+
+  const columns: GridColDef[] = [
+    {field: 'brand', headerName: 'Brand', width: 200,},
+    {field: 'model', headerName: 'Model', width: 200,},
+    {field: 'color', headerName: 'Color', width: 200,},
+    {field: 'registrationNumber', headerName: 'RegistrationNumber', width: 200,},
+    {field: 'modelYear', headerName: 'ModelYear', width: 200,},
+    {field: 'price', headerName: 'Price', width: 200,},
+    {
+      field: 'edit',
+      headerName: '',
+      sortable: false,
+      filterable: false,
+      disableColumnMenu: true,
+      renderCell: (params: GridCellParams) => <EditCar cardata={params.row}/>
+    },
+    {
+      field: 'delete',
+      headerName: '',
+      sortable: false,
+      filterable: false,
+      disableColumnMenu: true,
+      renderCell: (params: GridCellParams) => (
+        <Button color="error"
+          onClick={() => {
+            if(confirm(`${params.row.brand}의 ${params.row.color} ${params.row.model}을(를) 삭제하시겠습니까?`)) {
+              mutate(params.row._links.self.href)
+            }
+          }}
+          >Delete
+        </Button>
+      )
+    }
+  ]
+  
+  const {data, error, isSuccess} = useQuery({
+    queryKey: ['cars'],
+    queryFn: getCars
+  });
+
+  const {mutate} = useMutation(deleteCar, {
+    onSuccess: () => {
+      setOpen(true);
+      queryClient.invalidateQueries({queryKey: ['cars']});
+    },
+    onError: err => {
+      console.log(err);
+    },
+  })
+
+  if (!isSuccess) {
+    return <span>Loading ...</span>
+  }
+  else if (error) {
+    return <span>자동차 데이터를 가져오던 중 오류가 발생했습니다 ...😊</span>
+  }
+  else {
+    return (
+      <>
+        <AddCar/>
+        <DataGrid
+          rows={data}
+          columns={columns}
+          disableRowSelectionOnClick={true}
+          getRowId={row => row._links.self.href}
+          slots={{toolbar: GridToolbar}}
+        />
+        <Snackbar
+          open={open}
+          autoHideDuration={2000}
+          onClose={() => setOpen(false)}
+          message='해당 차량 정보가 삭제되었습니다.☕'
+        />
+      </>
+    );
+  }
+}
