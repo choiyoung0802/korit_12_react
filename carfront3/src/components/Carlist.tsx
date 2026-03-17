80ee24358faa6e9c9c410f9bf7b075bd80d1e35f
choiyoung0802 <mj12579632@gmail.com>
2026-03-17 15:14:43 +0900

    feat: 20260317 login / logout

---
+++ carfront3/src/components/Carlist.tsx
@@ -0,0 +1,99 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { getCars, deleteCar } from "../api/Carapi";
+import { DataGrid, GridCellParams, GridColDef, GridToolbar } from "@mui/x-data-grid";
+import { Snackbar, Tooltip, IconButton, Button, Stack } from "@mui/material";
+import DeleteForeverRoundedIcon from "@mui/icons-material/DeleteForeverRounded";
+import { useState } from "react";
+import AddCar from "./AddCar";
+import EditCar from "./EditCar";
+
+type CarlistProps = {
+  logout?: () => void;
+}
+
+export default function Carlist({logout}: CarlistProps) {
+  const [open, setOpen] = useState(false);
+  const queryClient = useQueryClient();
+
+  const columns: GridColDef[] = [
+    {field: 'brand', headerName: 'Brand', width: 200,},
+    {field: 'model', headerName: 'Model', width: 200,},
+    {field: 'color', headerName: 'Color', width: 200,},
+    {field: 'registrationNumber', headerName: 'RegistrationNumber', width: 200,},
+    {field: 'modelYear', headerName: 'ModelYear', width: 200,},
+    {field: 'price', headerName: 'Price', width: 200,},
+    {
+      field: 'edit',
+      headerName: '',
+      sortable: false,
+      filterable: false,
+      disableColumnMenu: true,
+      renderCell: (params: GridCellParams) => <EditCar cardata={params.row}/>
+    },
+    {
+      field: 'delete',
+      headerName: '',
+      sortable: false,
+      filterable: false,
+      disableColumnMenu: true,
+      renderCell: (params: GridCellParams) => (
+        <Tooltip title='delete car'>
+          <IconButton aria-label="delete" size="small" color="warning"
+            onClick={() => {
+              if(confirm(`${params.row.brand}의 ${params.row.color} ${params.row.model}을 삭제하시겠습니까?`)) {
+                mutate(params.row._links.self.href);
+              }
+            }}
+          >
+            <DeleteForeverRoundedIcon fontSize="small"/>
+          </IconButton>
+        </Tooltip>
+      )
+    }
+  ]
+  
+  const {data, error, isSuccess} = useQuery({
+    queryKey: ['cars'],
+    queryFn: getCars
+  });
+
+  const {mutate} = useMutation(deleteCar, {
+    onSuccess: () => {
+      setOpen(true);
+      queryClient.invalidateQueries({queryKey: ['cars']});
+    },
+    onError: err => {
+      console.log(err);
+    },
+  })
+
+  if (!isSuccess) {
+    return <span>Loading ...</span>
+  }
+  else if (error) {
+    return <span>자동차 데이터를 가져오던 중 오류가 발생했습니다 ...😊</span>
+  }
+  else {
+    return (
+      <>
+        <Stack direction="row" alignItems="center" justifyContent="space-between">
+          <AddCar/>
+          <Button onClick={logout}>LOGOUT</Button>
+        </Stack>
+        <DataGrid
+          rows={data}
+          columns={columns}
+          disableRowSelectionOnClick={true}
+          getRowId={row => row._links.self.href}
+          slots={{toolbar: GridToolbar}}
+        />
+        <Snackbar
+          open={open}
+          autoHideDuration={2000}
+          onClose={() => setOpen(false)}
+          message='해당 차량 정보가 삭제되었습니다.☕'
+        />
+      </>
+    );
+  }
+}
